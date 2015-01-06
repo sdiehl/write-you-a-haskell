065e55d02d570f6935415a201e68ee15a2818e12
Stephen Diehl <stephen.m.diehl@gmail.com>
2015-01-06 15:32:02 -0500

    emptyDef -> langDef

--- chapter3/calc/Parser.hs
+++ chapter3/calc/Parser.hs
@@ -13,9 +13,23 @@ import qualified Text.Parsec.Token as Tok
 
 import Data.Functor.Identity
 
+langDef :: Tok.LanguageDef ()
+langDef = Tok.LanguageDef
+  { Tok.commentStart    = "{-"
+  , Tok.commentEnd      = "-}"
+  , Tok.commentLine     = "--"
+  , Tok.nestedComments  = True
+  , Tok.identStart      = letter
+  , Tok.identLetter     = alphaNum <|> oneOf "_'"
+  , Tok.opStart         = oneOf ":!#$%&*+./<=>?@\\^|-~"
+  , Tok.opLetter        = oneOf ":!#$%&*+./<=>?@\\^|-~"
+  , Tok.reservedNames   = []
+  , Tok.reservedOpNames = []
+  , Tok.caseSensitive   = True
+  }
 
 lexer :: Tok.TokenParser ()
-lexer = Tok.makeTokenParser emptyDef
+lexer = Tok.makeTokenParser langDef
 
 parens :: Parser a -> Parser a
 parens = Tok.parens lexer
