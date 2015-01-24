2a77bb041476c2f19a2172e7f539d8a544674fba
Stephen Diehl <stephen.m.diehl@gmail.com>
2015-01-24 14:37:01 -0500

    update rss

--- misc/rssgen.py
+++ misc/rssgen.py
@@ -13,59 +13,66 @@ aug = datetime.datetime(2015, 8, 1, 0, 0)
 pages = [
    PyRSS2Gen.RSSItem(
      title = "Introduction",
-     link = "http://dev.stephendiehl.com/fun/introduction.html",
+     link = "http://dev.stephendiehl.com/fun/000_introduction.html",
      description = "",
-     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/introduction.html"),
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/000_introduction.html"),
      pubDate = jan + datetime.timedelta(1) ),
 
    PyRSS2Gen.RSSItem(
      title = "Haskell Basics",
-     link = "http://dev.stephendiehl.com/fun/basics.html",
+     link = "http://dev.stephendiehl.com/fun/001_basics.html",
      description = "",
-     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/basics.html"),
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/001_basics.html"),
      pubDate = jan + datetime.timedelta(2) ),
 
    PyRSS2Gen.RSSItem(
      title = "Parsing",
-     link = "http://dev.stephendiehl.com/fun/parsers.html",
+     link = "http://dev.stephendiehl.com/fun/002_parsers.html",
      description = "",
-     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/parsers.html"),
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/002_parsers.html"),
      pubDate = jan + datetime.timedelta(3)),
 
    PyRSS2Gen.RSSItem(
      title = "Lambda Calculus",
-     link = "http://dev.stephendiehl.com/fun/lambda_calculus.html",
+     link = "http://dev.stephendiehl.com/fun/003_lambda_calculus.html",
      description = "",
-     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/lambda_calculus.html"),
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/003_lambda_calculus.html"),
      pubDate = jan + datetime.timedelta(4)),
 
    PyRSS2Gen.RSSItem(
      title = "Type Systems",
-     link = "http://dev.stephendiehl.com/fun/type_systems.html",
+     link = "http://dev.stephendiehl.com/fun/004_type_systems.html",
      description = "",
-     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/type_systems.html"),
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/004_type_systems.html"),
      pubDate = jan + datetime.timedelta(5)),
 
    PyRSS2Gen.RSSItem(
      title = "Evaluation",
-     link = "http://dev.stephendiehl.com/fun/evaluation.html",
+     link = "http://dev.stephendiehl.com/fun/005_evaluation.html",
      description = "",
-     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/evaluation.html"),
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/005_evaluation.html"),
      pubDate = jan + datetime.timedelta(6)),
 
    PyRSS2Gen.RSSItem(
      title = "Hindley-Milner Inference",
-     link = "http://dev.stephendiehl.com/fun/hindley_milner.html",
+     link = "http://dev.stephendiehl.com/fun/006_hindley_milner.html",
      description = "",
-     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/hindley_milner.html"),
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/006_hindley_milner.html"),
      pubDate = jan + datetime.timedelta(7)),
 
    PyRSS2Gen.RSSItem(
      title = "Design of ProtoHaskell",
-     link = "http://dev.stephendiehl.com/fun/path.html",
+     link = "http://dev.stephendiehl.com/fun/007_path.html",
      description = "",
-     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/path.html"),
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/007_path.html"),
      pubDate = jan + datetime.timedelta(8)),
+
+   PyRSS2Gen.RSSItem(
+     title = "Extended Parser",
+     link = "http://dev.stephendiehl.com/fun/008_extended_parser.html",
+     description = "",
+     guid = PyRSS2Gen.Guid("http://dev.stephendiehl.com/fun/008_extended_parser.html"),
+     pubDate = datetime.datetime(2015, 1, 24, 14, 30, 28, 996866))
 ]
 
 rss = PyRSS2Gen.RSS2(
