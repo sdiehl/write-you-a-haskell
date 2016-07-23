033d0ae47720ef72d78a816d420412989043b492
Stephen Webber <steve@falco>
2016-07-23 18:34:57 -0500

    fix names for ski combinators

--- chapter4/lambda.hs
+++ chapter4/lambda.hs
@@ -6,6 +6,6 @@ data Expr
   | Lam Name Expr
 
 s, k, i :: Expr
-s = Lam "x" (Var "x")
+i = Lam "x" (Var "x")
 k = Lam "x" (Lam "y" (Var "x"))
-i = Lam "x" (Lam "y" (Lam "z" (App (App (Var "x") (Var "z")) (App (Var "y") (Var "z")))))
+s = Lam "x" (Lam "y" (Lam "z" (App (App (Var "x") (Var "z")) (App (Var "y") (Var "z")))))
